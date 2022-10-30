4cd11d48fdeb78c4110ff4eefdaea8b7f014a33e
zhy76 <958474674@qq.com>
2022-10-30 20:33:06 +0800

    add 784
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ golang/784. 字母大小写全排列.go
@@ -0,0 +1,18 @@
+func letterCasePermutation(s string) (ans[]string) {
+    var dfs func([]byte,int)
+    dfs=func(s []byte,pos int){
+        for pos <len(s) && unicode.IsDigit(rune(s[pos])){
+            pos++
+        }
+        if pos ==len(s){
+            ans=append(ans,string(s))
+            return
+        }
+        dfs(s,pos+1)
+        s[pos]^=32
+        dfs(s,pos+1)
+        s[pos]^=32
+    }
+    dfs([]byte(s),0)
+    return
+}
