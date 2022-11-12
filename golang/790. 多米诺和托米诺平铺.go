35a603eec155550805e25ebba5c36eeedb9a6fd2
zhy76 <958474674@qq.com>
2022-11-12 23:01:28 +0800

    add 790
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ golang/790. 多米诺和托米诺平铺.go
@@ -0,0 +1,12 @@
+func numTilings(n int) int {
+    const mod int = 1e9 + 7
+    dp := make([][4]int, n+1)
+    dp[0][3] = 1
+    for i := 1; i <= n; i++ {
+        dp[i][0] = dp[i-1][3]
+        dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % mod
+        dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % mod
+        dp[i][3] = (((dp[i-1][0]+dp[i-1][1])%mod+dp[i-1][2])%mod + dp[i-1][3]) % mod
+    }
+    return dp[n][3]
+}
