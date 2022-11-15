5a30fb46a616dd03472e0da3be86a7ddb1a3f176
zhy76 <958474674@qq.com>
2022-11-15 23:34:36 +0800

    add 1710
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ cpp/1710. 卡车上的最大单元数.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        sort(boxTypes.begin(), boxTypes.end(), [](const vector<int> &a, const vector<int> &b) {
+            return a[1] > b[1];
+        });
+        int res = 0;
+        for (auto &boxType : boxTypes) {
+            int numberOfBoxes = boxType[0];
+            int numberOfUnitsPerBox = boxType[1];
+            if (numberOfBoxes < truckSize) {
+                res += numberOfBoxes * numberOfUnitsPerBox;
+                truckSize -= numberOfBoxes;
+            } else {
+                res += truckSize * numberOfUnitsPerBox;
+                break;
+            }
+        }
+        return res;
+    }
+};
