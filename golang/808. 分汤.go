158a03faf6a7bd9b0763b05e8bed1e49e0fa9cdb
zhy76 <958474674@qq.com>
2022-11-21 16:26:37 +0800

    add 808
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ golang/808. 分汤.go
@@ -0,0 +1,28 @@
+func soupServings(n int) float64 {
+    n = (n+24)/25
+    if n>=179{
+        return 1.0
+    }
+    dp:=make([][]float64,n+1)
+    for i:=range dp{
+        dp[i]=make([]float64,n+1)
+    }
+    dp[0][0]=0.5
+    for i:=1;i<=n;i++{
+        dp[0][i]=1
+    }
+    for i:=1;i<=n;i++{
+        for j:=1;j<=n;j++{
+            dp[i][j]=(dp[max(0, i-4)][j] + dp[max(0, i-3)][max(0, j-1)] +
+                dp[max(0, i-2)][max(0, j-2)] + dp[max(0, i-1)][max(0, j-3)]) / 4
+        }
+    }
+    return dp[n][n]
+}
+
+func max(i,j int) int{
+    if(i>j){
+        return i
+    }
+    return j
+}
