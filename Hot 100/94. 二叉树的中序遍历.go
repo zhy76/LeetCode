137d05806a8d105d713c8ddc305fe392b14c3f59
zhy76 <958474674@qq.com>
2023-05-22 21:48:13 +0800

    add 94
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ Hot 100/94. 二叉树的中序遍历.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func inorderTraversal(root *TreeNode) []int {
+	var ans []int
+	var inorder func(node *TreeNode)
+	inorder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		inorder(node.Left)
+		ans = append(ans, node.Val)
+		inorder(node.Right)
+	}
+	inorder(root)
+	return ans
+}
