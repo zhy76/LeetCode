eaf4b5adb420561c8d4c83d453c22e52bcafc656
zhy76 <958474674@qq.com>
2023-10-18 01:31:49 +0800

    141. 环形链表
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ golang/141. 环形链表.go
@@ -0,0 +1,14 @@
+func hasCycle(head *ListNode) bool {
+    if head == nil || head.Next == nil {
+        return false
+    }
+    slow, fast := head, head.Next
+    for fast != slow {
+        if fast == nil || fast.Next == nil {
+            return false
+        }
+        slow = slow.Next
+        fast = fast.Next.Next
+    }
+    return true
+}
