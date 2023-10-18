6bb52d35e9b31c49577671f8254ea7720f706262
zhy76 <958474674@qq.com>
2023-10-18 15:04:02 +0800

    27. 移除元素
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ cpp/27. 移除元素.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        if(nums.size() == 0) {
+            return 0;
+        }
+        int slow=0, fast=0;
+        while(fast<nums.size()){
+            if(nums[fast]!=val){
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+};
---
+++ golang/27. 移除元素.go
@@ -0,0 +1,14 @@
+func removeElement(nums []int, val int) int {
+    if len(nums) == 0 {
+        return 0
+    }
+    slow, fast := 0, 0
+    for fast < len(nums) {
+        if nums[fast] != val {
+            nums[slow] = nums[fast]
+            slow++
+        }
+        fast++
+    }
+    return slow
+}
