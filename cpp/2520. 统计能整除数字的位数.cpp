877ef83ac5e9ffd2c070ca4f0d76028d5a21b4cc
zhy76 <958474674@qq.com>
2023-10-26 00:46:49 +0800

    2520. 统计能整除数字的位数
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ cpp/2520. 统计能整除数字的位数.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countDigits(int num) {
+        int t=num;
+        int cnt=0;
+        while(num!=0){
+            int x=num%10;
+            if(t%x==0){
+                cnt++;
+            }
+            num/=10;
+        }
+        return cnt;
+    }
+};
---
+++ golang/2520. 统计能整除数字的位数.go
@@ -0,0 +1,10 @@
+func countDigits(num int) int {
+    t, res := num, 0
+    for t != 0 {
+        if num % (t % 10) == 0 {
+            res += 1
+        }
+        t /= 10
+    }
+    return res
+}
