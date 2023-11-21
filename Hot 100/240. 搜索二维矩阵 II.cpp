48c87bebd3a7ff2d141027f93e0cf636f643edef
zhy76 <958474674@qq.com>
2023-11-21 22:56:24 +0800

    240. 搜索二维矩阵 II
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ Hot 100/240. 搜索二维矩阵 II.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size(),n=matrix[0].size();
+        int x=0,y=n-1;
+        while(x<m && y>=0){
+            if(matrix[x][y] == target) return true;
+            else if(matrix[x][y]>target){
+                y--;
+            }else{
+                x++;
+            }
+        }
+        return false;
+    }
+};
