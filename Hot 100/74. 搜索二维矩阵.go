80e7c33a9036bb85a0b1307876d8e9a85c0b71cb
zhy76 <958474674@qq.com>
2023-11-28 00:41:35 +0800

    74. 搜索二维矩阵
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ Hot 100/74. 搜索二维矩阵.go
@@ -0,0 +1,14 @@
+func searchMatrix(matrix [][]int, target int) bool {
+    m, n := len(matrix), len(matrix[0])
+    i, j := 0, n-1
+    for i<m && j>=0 {
+        if matrix[i][j] == target {
+            return true
+        } else if matrix[i][j] < target {
+            i++
+        } else {
+            j--
+        }
+    }
+    return false
+}
