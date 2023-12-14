ef6bd9466ed5bc79cfc7e9662450360dc03b68eb
zhy76 <958474674@qq.com>
2023-12-14 21:49:11 +0800

    567. 字符串的排列
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ cpp/567. 字符串的排列.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char, int> need;
+        unordered_map<char, int> window;
+        int left=0, right=0;
+        for(auto c : s1) {
+            need[c]++;
+        }
+        int valid = 0;
+        while(right < s2.length()){
+            char c = s2[right];
+            right++;
+            if(need.count(c)) {
+                window[c]++;
+                if(window[c] == need[c]) {
+                    valid++;
+                }
+            }
+            while(left < right && right - left >= s1.length()) {
+                if(valid == need.size()) {
+                    return true;
+                }
+                char d = s2[left];
+                left++;
+                if(need.count(d)) {
+                    if(window[d] == need[d]) {
+                        valid--;
+                    }
+                    window[d]--;
+                }
+            }
+        }
+        return false;
+    }
+};
