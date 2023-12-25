6966386d206dd8aee9b68e9c5b794ee3c1329f96
zhy76 <958474674@qq.com>
2023-12-25 22:43:21 +0800

    450. 删除二叉搜索树中的节点
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ cpp/450. 删除二叉搜索树中的节点.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root == nullptr) return nullptr;
+        if(root->val == key) {
+            if(root->left == nullptr) return root->right;
+            if(root->right == nullptr) return root->left;
+            TreeNode* minNode = getMin(root->right);
+            root->right = deleteNode(root->right, minNode->val);
+            minNode->left = root->left;
+            minNode->right = root->right;
+            root=minNode;
+        } else if(root->val < key) {
+            root->right = deleteNode(root->right, key);
+        }else if(root->val > key) {
+            root->left = deleteNode(root->left, key);
+        }
+        return root;
+    }
+    TreeNode* getMin(TreeNode* root) {
+        if(root->left != nullptr) return getMin(root->left);
+        return root;
+    }
+};
