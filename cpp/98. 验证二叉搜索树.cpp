a5ff45b6cac6c3f345cb755ff8cdc1e2de91e06d
zhy76 <958474674@qq.com>
2023-12-25 21:48:41 +0800

    98. 验证二叉搜索树
    Signed-off-by: zhy76 <958474674@qq.com>

--- cpp/98. 验证二叉搜索树.cpp
+++ cpp/98. 验证二叉搜索树.cpp
@@ -11,18 +11,16 @@
  */
 class Solution {
 public:
-    int sum = 0;
-    void traverse(TreeNode* root) {
+    bool isValidBST(TreeNode* root, long long min, long long max){
         if(root == nullptr) {
-            return;
+            return true;
         }
-        traverse(root->right);
-        sum += root->val;
-        root->val = sum;
-        traverse(root->left);
+        if(root -> val <= min || root -> val >= max) {
+            return false;
+        }
+        return isValidBST(root->left, min, root->val) && isValidBST(root->right, root->val, max);
     }
-    TreeNode* convertBST(TreeNode* root) {
-        traverse(root);
-        return root;
+    bool isValidBST(TreeNode* root) {
+        return isValidBST(root, LONG_MIN, LONG_MAX);
     }
 };
