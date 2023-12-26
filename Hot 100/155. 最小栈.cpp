45943eb59877eade086975bb9a56a9993e4519ea
zhy76 <958474674@qq.com>
2023-12-26 22:42:54 +0800

    155. 最小栈
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ Hot 100/155. 最小栈.cpp
@@ -0,0 +1,36 @@
+class MinStack {
+private:
+    stack<int> x_stack;
+    stack<int> min_stack;
+public:
+    MinStack() {
+        min_stack.push(INT_MAX);
+    }
+    
+    void push(int val) {
+        x_stack.push(val);
+        min_stack.push(min(min_stack.top(),val));
+    }
+    
+    void pop() {
+        x_stack.pop();
+        min_stack.pop();
+    }
+    
+    int top() {
+        return x_stack.top();
+    }
+    
+    int getMin() {
+        return min_stack.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
