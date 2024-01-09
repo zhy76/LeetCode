c59b6952730248a1e0466172af4c0bad3580b442
zhy76 <958474674@qq.com>
2024-01-09 14:05:37 +0800

    25. K 个一组翻转链表
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ Hot 100/25. K 个一组翻转链表.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseAB(ListNode* a, ListNode* b) {
+        ListNode* pre = nullptr, *cur = a, *nxt = a;
+        while(cur != b) {
+            nxt = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = nxt;
+        } 
+        return pre;
+    }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(head == nullptr) return head;
+        ListNode* a, *b;
+        a=b=head;
+        for(int i=0; i<k; i++) {
+            if(b == nullptr) return head;
+            b = b->next;
+        }
+        ListNode* newHead = reverseAB(a, b);
+        a->next = reverseKGroup(b, k);
+        return newHead;
+    }
+};
