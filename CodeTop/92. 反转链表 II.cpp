094b45948e303b08babd65c7a6b6a503fcd17710
zhy76 <958474674@qq.com>
2024-01-20 01:09:49 +0800

    92. 反转链表 II
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ CodeTop/92. 反转链表 II.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* succ;
+    ListNode* reverseN(ListNode* head, int n) {
+        if(n == 1) {
+            succ = head->next;
+            return head;
+        }
+        ListNode* last = reverseN(head->next, n-1);
+        head->next->next = head;
+        head->next = succ;
+        return last;
+    }
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(left == 1) {
+            return reverseN(head, right);
+        }
+        head->next = reverseBetween(head->next, left - 1, right - 1);
+        return head;
+    }
+};
