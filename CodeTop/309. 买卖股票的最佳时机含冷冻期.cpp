343fffd488bc60af0f4875a01e56b5b8e21c9aa9
zhy76 <958474674@qq.com>
2024-01-21 16:20:23 +0800

    309. 买卖股票的最佳时机含冷冻期
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ CodeTop/309. 买卖股票的最佳时机含冷冻期.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int> > dp(n, vector<int>(2, 0));
+        for(int i=0; i<n; i++) {
+            if(i == 0) {
+                dp[i][0] = 0;
+                dp[i][1] = -prices[i];
+                continue;
+            }
+            if(i - 2 == -1) {
+                dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i]);
+                dp[i][1] = max(dp[i-1][1], -prices[i]);
+                continue;
+            }
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i]);
+            dp[i][1] = max(dp[i-1][1], dp[i-2][0] - prices[i]);
+        }
+        return dp[n-1][0];
+    }
+};
