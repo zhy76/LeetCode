80cfd6962aeb6aae31c509d9d9090952359ae962
zhy76 <958474674@qq.com>
2024-01-21 12:08:15 +0800

    LCR 194. 二叉树的最近公共祖先
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ 剑指offer/LCR 194. 二叉树的最近公共祖先.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* find(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL) return root;
+        if(root == p || root == q) return root;
+        TreeNode* left = find(root->left, p, q);
+        TreeNode* right = find(root->right, p, q);
+        if(left != NULL && right != NULL) return root;
+        return left == NULL? right:left;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return find(root, p, q);
+    }
+};
