aab2dfa43ba5ad55e7a64a8e200c742361b531e4
zhy76 <958474674@qq.com>
2024-01-30 23:39:40 +0800

    232. 用栈实现队列
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ CodeTop/232. 用栈实现队列.cpp
@@ -0,0 +1,43 @@
+class MyQueue {
+private:
+    stack<int> st1;
+    stack<int> st2;
+public:
+    MyQueue() {
+
+    }
+    
+    void push(int x) {
+        st1.push(x);
+    }
+    
+    int pop() {
+        int x = peek();
+        st2.pop();
+        return x;
+    }
+    
+    int peek() {
+        if(st2.empty()) {
+            while(!st1.empty()){
+                st2.push(st1.top());
+                st1.pop();
+            }
+        } 
+        int x = st2.top();
+        return x;
+    }
+    
+    bool empty() {
+        return st1.empty() && st2.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
