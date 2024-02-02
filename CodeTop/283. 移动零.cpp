9f40597e3198bcbab8d098436204a04ede0b10d3
zhy76 <958474674@qq.com>
2024-02-02 11:34:46 +0800

    283. 移动零
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ CodeTop/283. 移动零.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int l = 0, r = 0;
+        while(r < nums.size()) {
+            if(nums[r] == 0) {
+                r++;
+                continue;
+            } else {
+                swap(nums[l], nums[r]);
+                l++;
+                r++:
+            }
+        }
+    }
+};
