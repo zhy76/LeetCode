0fc25011c298ccfe07b4a2b39d64bf6627aa6699
zhy76 <958474674@qq.com>
2024-02-21 22:03:03 +0800

    287. 寻找重复数
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ CodeTop/287. 寻找重复数.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = 0, fast = 0;
+        while(true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow == fast) break; 
+        }
+        slow = 0;
+        while(true) {
+            slow = nums[slow];
+            fast = nums[fast];
+            if(slow == fast) return slow;
+        }
+        return -1;
+    }
+};
