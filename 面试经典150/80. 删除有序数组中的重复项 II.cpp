68ec9d5c0cd6d3f4201e3335b299bf2b9b3fdd66
zhy76 <958474674@qq.com>
2024-02-27 22:10:12 +0800

    80. 删除有序数组中的重复项 II
    Signed-off-by: zhy76 <958474674@qq.com>

--- 面试经典150/80. 删除有序数组中的重复项 II.cpp
+++ 面试经典150/80. 删除有序数组中的重复项 II.cpp
@@ -17,3 +17,21 @@ public:
         return nums.size();
     }
 };
+
+// 快慢指针
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if(n <= 2) return n;
+        int slow = 2, fast = 2;
+        while(fast < n) {
+            if(nums[slow - 2] != nums[fast]) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+};
