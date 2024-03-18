8167f1c67964c4174d23ec0c92d77a01b4b8924d
zhy76 <958474674@qq.com>
2024-03-18 22:48:59 +0800

    877. 石子游戏
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ 刷题/CodeTop/877. 石子游戏.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(2, 0)));
+        for(int i = 0; i < n; i++) {
+            dp[i][i][0] = piles[i];
+            dp[i][i][1] = 0;
+        }
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = i + 1; j < n; j++) {
+                int left = piles[i] + dp[i + 1][j][1];
+                int right = piles[j] + dp[i][j - 1][1];
+                if(left > right) {
+                    dp[i][j][0] = left;
+                    dp[i][j][1] = dp[i + 1][j][0];
+                } else {
+                    dp[i][j][0] = right;
+                    dp[i][j][1] = dp[i][j - 1][0];
+                }
+            }
+        }
+        return dp[0][n-1][0] > dp[0][n - 1][1]; 
+    }
+};
