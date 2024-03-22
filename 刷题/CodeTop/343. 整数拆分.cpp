39cf9d056a9a4b87e0f0e22f50e9cb29b067c4e1
zhy76 <958474674@qq.com>
2024-03-22 17:17:06 +0800

    343. 整数拆分
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ 刷题/CodeTop/343. 整数拆分.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector<int> dp(n + 1, 0);
+        if(n <= 3) return n - 1; 
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 2;
+        for(int i = 4; i <= n; i++) {
+            for(int j = 1; j <= i / 2; j++) {
+                dp[i] = max(dp[i], j * max((i - j), dp[i - j]));
+            }
+        }
+        return dp[n];
+    }
+};
