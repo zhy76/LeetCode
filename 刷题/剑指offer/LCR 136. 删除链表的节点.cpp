d45b37ca3e17c84de7c59aa38698931daa530c96
zhy76 <958474674@qq.com>
2024-03-22 22:02:24 +0800

    LCR 136. 删除链表的节点
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ 刷题/剑指offer/LCR 136. 删除链表的节点.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteNode(ListNode* head, int val) {
+        if(head->val == val) return head->next;
+        ListNode* pre = head;
+        ListNode* cur = head->next;
+        while(cur != NULL) {
+            if(cur->val == val) {
+                pre->next = cur->next;
+                break;
+            }
+            pre = cur;
+            cur = cur->next;
+        }
+        return head;
+    }
+};
