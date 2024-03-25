ecf35ea9c17ade93d7f4705953636a1bb1197205
zhy76 <958474674@qq.com>
2024-03-25 23:19:11 +0800

    LCR 161. 连续天数的最高销售额
    Signed-off-by: zhy76 <958474674@qq.com>

---
+++ 刷题/剑指offer/LCR 161. 连续天数的最高销售额.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSales(vector<int>& sales) {
+        int n = sales.size();
+        vector<int> dp(n, 0);
+        dp[0] = sales[0];
+        for(int i = 1; i < n; i++) {
+            dp[i] = max(dp[i - 1] + sales[i], sales[i]);
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
+};
