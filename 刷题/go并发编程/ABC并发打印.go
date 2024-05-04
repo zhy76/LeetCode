abde6f29fbe22d8055e6f981f7effee60aca36a0
zhy76 <958474674@qq.com>
2024-05-04 20:21:37 +0800

    LCR 098. 不同路径
    Signed-off-by: zhy76 <958474674@qq.com>

--- 刷题/go并发编程/ABC并发打印.go
+++ 刷题/go并发编程/ABC并发打印.go
@@ -7,50 +7,82 @@ import (
     "sync"
 )
 
-func PrintA(wg *sync.WaitGroup, chA, chB chan struct{}) {
-    for i := 0; i < 10; i++ {
-        <-chA
-        fmt.Println("A")
-        chB <- struct{}{}
+func printChars(wg *sync.WaitGroup, chIn, chOut chan struct{}, char string, count int) {
+    defer wg.Done() // Ensure wg.Done() is called even if panic occurs
+    for i := 0; i < count; i++ {
+        <-chIn // Wait for signal
+        fmt.Println(char)
+        chOut <- struct{}{} // Signal next goroutine
     }
-    wg.Done()
-}
-
-func PrintB(wg *sync.WaitGroup, chB, chC chan struct{}) {
-    for i := 0; i < 10; i++ {
-        <-chB
-        fmt.Println("B")
-        chC <- struct{}{}
-    }
-    wg.Done()
-}
-
-func PrintC(wg *sync.WaitGroup, chC, chA chan struct{}) {
-    for i := 0; i < 10; i++ {
-        <-chC
-        fmt.Println("C")
-        chA <- struct{}{}
-    }
-    wg.Done()
 }
 
 func main() {
-    wg := sync.WaitGroup{}
-    chA := make(chan struct{}, 1)
-    chB := make(chan struct{}, 1)
-    chC := make(chan struct{}, 1)
+    var wg sync.WaitGroup
+    chA, chB, chC := make(chan struct{}, 1), make(chan struct{}, 1), make(chan struct{}, 1)
+    count := 10 // Number of iterations for each goroutine
 
-    chA <- struct{}{}
+    chA <- struct{}{} // Start the chain
 
-    wg.Add(1)
-    go PrintA(&wg, chA, chB)
+    wg.Add(3)
+    go printChars(&wg, chA, chB, "A", count)
+    go printChars(&wg, chB, chC, "B", count)
+    go printChars(&wg, chC, chA, "C", count)
 
-    wg.Add(1)
-    go PrintB(&wg, chB, chC)
-
-    wg.Add(1)
-    go PrintC(&wg, chC, chA)
-
-    wg.Wait()
+    wg.Wait() // Wait for all goroutines to finish
     fmt.Println("Success.")
 }
+
+// package main
+
+// import (
+//     "fmt"
+//     "sync"
+// )
+
+// func PrintA(wg *sync.WaitGroup, chA, chB chan struct{}) {
+//     for i := 0; i < 10; i++ {
+//         <-chA
+//         fmt.Println("A")
+//         chB <- struct{}{}
+//     }
+//     wg.Done()
+// }
+
+// func PrintB(wg *sync.WaitGroup, chB, chC chan struct{}) {
+//     for i := 0; i < 10; i++ {
+//         <-chB
+//         fmt.Println("B")
+//         chC <- struct{}{}
+//     }
+//     wg.Done()
+// }
+
+// func PrintC(wg *sync.WaitGroup, chC, chA chan struct{}) {
+//     for i := 0; i < 10; i++ {
+//         <-chC
+//         fmt.Println("C")
+//         chA <- struct{}{}
+//     }
+//     wg.Done()
+// }
+
+// func main() {
+//     wg := sync.WaitGroup{}
+//     chA := make(chan struct{}, 1)
+//     chB := make(chan struct{}, 1)
+//     chC := make(chan struct{}, 1)
+
+//     chA <- struct{}{}
+
+//     wg.Add(1)
+//     go PrintA(&wg, chA, chB)
+
+//     wg.Add(1)
+//     go PrintB(&wg, chB, chC)
+
+//     wg.Add(1)
+//     go PrintC(&wg, chC, chA)
+
+//     wg.Wait()
+//     fmt.Println("Success.")
+// }
--- 刷题/go并发编程/AB并发打印.go
+++ 刷题/go并发编程/AB并发打印.go
@@ -1,28 +1,54 @@
+// A和B两个协程，B协程等待A协程的ready（条件变量）为true，然后B要做一个process；A等待B的process完成之后打印一个“hello”，并且把ready设为true；
 package main
 
-// A和B两个线程，B线程等待A线程的ready（条件变量）为true，然后B要做一个process；A等待B的process完成之后打印一个“hello”，并且把ready设为true；
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 func main() {
+	var mutex sync.Mutex
+	cond := sync.NewCond(&mutex)
+	ready := false
+
 	var wg sync.WaitGroup
-	ready := make(chan bool)
-	done := make(chan bool)
-	wg.Add(1)
+	wg.Add(2)
+
+	// A goroutine
 	go func() {
 		defer wg.Done()
-		<-ready
-		fmt.Println("Hello A")
-		done <- true
+
+		// 等待B处理完成
+		mutex.Lock()
+		for !ready {
+			cond.Wait()
+		}
+		mutex.Unlock()
+
+		fmt.Println("hello")
+		ready = true // 在实际场景中，可能需要用其他方式通知B或其他协程继续工作
 	}()
-	wg.Add(1)
+
+	// B goroutine
 	go func() {
 		defer wg.Done()
-		fmt.Println("Hello B")
-		ready <- true
-		<-done
+
+		// 假设的处理过程
+		process()
+
+		mutex.Lock()
+		ready = true
+		cond.Broadcast() // 通知所有等待的协程条件已经满足
+		mutex.Unlock()
 	}()
+
 	wg.Wait()
+}
+
+func process() {
+	// 模拟B的处理过程，包括3秒的延迟
+	fmt.Println("B is processing")
+	time.Sleep(3 * time.Second) // 添加3秒延迟
+	fmt.Println("B finished processing")
 }
